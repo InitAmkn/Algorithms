6fcc94acc4df52eac726834d896cac0460ff406f
Alexander Mamykin <109105189+InitAmkn@users.noreply.github.com>
2023-07-21 18:53:21 +0300

    HeapSort is work

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -5,40 +5,62 @@ public class HeapSort {
 
     //Реализовать алгоритм пирамидальной сортировки (сортировка кучей).
 
-    private boolean shiftMaxInFamily(Integer[] arr, int indexRoot) {
+
+    public void sort(Integer[] arr) {
+        sort(arr, 0);
+    }
+
+    private void sort(Integer[] arr, int skipPosition) {
+        if (skipPosition >= arr.length) return;
+        for (int j = arr.length - 1 - skipPosition; j >= 0; j--) {
+            shiftMaxInFamily(arr, j, skipPosition);
+        }
+
+        int temp = arr[0];
+        arr[0] = arr[(arr.length - 1) - skipPosition];
+        arr[(arr.length - 1) - skipPosition] = temp;
+
+        skipPosition++;
+        sort(arr, skipPosition);
+
+    }
+
+    private void shiftMaxInFamily(Integer[] arr, int indexRoot, int skipPosition) {
         int indexLeft = (indexRoot * 2) + 1;
         int indexRight = (indexRoot * 2) + 2;
 
-        if (arr[indexLeft] == null && arr[indexRight] == null) return false;
+        if (indexLeft >= arr.length - skipPosition && indexRight >= arr.length - skipPosition) return;
 
-        if (arr[indexLeft] == null && arr[indexRight] != null) {
+        if (indexRight < arr.length - skipPosition) {
             if (arr[indexRoot] < arr[indexRight]) {
                 int temp = arr[indexRoot];
                 arr[indexRoot] = arr[indexRight];
-                arr[indexRoot] = temp;
+                arr[indexRight] = temp;
             }
         }
-        if (arr[indexLeft] == null && arr[indexRight] != null) {
+        if (indexLeft < arr.length - skipPosition) {
             if (arr[indexRoot] < arr[indexLeft]) {
                 int temp = arr[indexRoot];
                 arr[indexRoot] = arr[indexLeft];
-                arr[indexRoot] = temp;
+                arr[indexLeft] = temp;
             }
         }
 
-        if (arr[indexLeft] != null && arr[indexRight] != null) {
-            int temp = arr[indexRoot];
+        if (indexLeft < arr.length - skipPosition && indexRight < arr.length - skipPosition) {
+
+            if (arr[indexRoot] < arr[indexRight]) {
+                int temp = arr[indexRoot];
+                arr[indexRoot] = arr[indexRight];
+                arr[indexRight] = temp;
+
+            }
             if (arr[indexRoot] < arr[indexLeft]) {
+                int temp = arr[indexRoot];
                 arr[indexRoot] = arr[indexLeft];
                 arr[indexLeft] = temp;
             }
-            if (arr[indexRoot] < arr[indexRight]){
-                arr[indexRoot] = arr[indexRight];
-                arr[indexRight] = temp;
-            }
-
         }
-        return true;
     }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -4,70 +4,22 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
-        Integer[] arr = new Integer[10];
+        HeapSort heap = new HeapSort();
+        Integer[] arr = new Integer[15];
+
         for (int i = 0; i < arr.length; i++) {
             Random r = new Random();
-            arr[i] = r.nextInt(10);
+
+            arr[i] = r.nextInt(100);
         }
         System.out.println(Arrays.toString(arr));
 
-        sort(arr,0);
-
+        heap.sort(arr);
         System.out.println(Arrays.toString(arr));
-    }
 
-    public static void sort(Integer[] arr, int skipPosition){
-        if (skipPosition == arr.length) return;
-        for (int i = (arr.length-1)-skipPosition; i >= 0; i--) {
-           shiftMaxInFamily(arr, i);
-        }
-        skipPosition++;
-
-        int temp = arr[0];
-        arr[0] = arr[arr.length-1];
-        arr[arr.length-1] = temp;
-
-        sort(arr, skipPosition);
 
     }
-    public static boolean shiftMaxInFamily(Integer[] arr, int indexRoot) {
-        int indexLeft = (indexRoot * 2) + 1;
-        int indexRight = (indexRoot * 2) + 2;
 
-        if (indexLeft >= arr.length && indexRight >= arr.length) return false;
-        else {
-            if (indexRight < arr.length) {
-                if (arr[indexRoot] < arr[indexRight]) {
-                    int temp = arr[indexRoot];
-                    arr[indexRoot] = arr[indexRight];
-                    arr[indexRight] = temp;
-                }
-            }
-            if (indexLeft < arr.length) {
-                if (arr[indexRoot] < arr[indexLeft]) {
-                    int temp = arr[indexRoot];
-                    arr[indexRoot] = arr[indexLeft];
-                    arr[indexLeft] = temp;
-                }
-            }
-        }
-
-        if (indexLeft < arr.length && indexRight < arr.length) {
-            int temp = arr[indexRoot];
-
-            if (arr[indexRoot] < arr[indexRight]) {
-                arr[indexRoot] = arr[indexRight];
-                arr[indexRight] = temp;
-                temp = arr[indexRoot];
-            }
-            if (arr[indexRoot] < arr[indexLeft]) {
-                arr[indexRoot] = arr[indexLeft];
-                arr[indexLeft] = temp;
-                temp = arr[indexRoot];
-            }
-        }
-        return true;
-    }
 
 
 }
