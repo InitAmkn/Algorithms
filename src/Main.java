58546e7fdfe89e73c5ac06c27e651ca697b4c2f2
Alexander Mamykin <109105189+InitAmkn@users.noreply.github.com>
2023-07-25 20:19:57 +0300

    add expand for LinkedList

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,17 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
-        HeapSort heap = new HeapSort();
-        Integer[] arr = new Integer[15];
 
-        for (int i = 0; i < arr.length; i++) {
-            Random r = new Random();
-
-            arr[i] = r.nextInt(100);
+        LinkedList ll = new LinkedList();
+        for (int i = 0;i<20 ;i++ )
+        {
+            ll.add(i);
         }
-        System.out.println(Arrays.toString(arr));
-
-        heap.sort(arr);
-        System.out.println(Arrays.toString(arr));
-
-
+        ll.print();
+        ll.expand1();
+        ll.print();
+        ll.expand();
+        ll.print();
     }
-
-
 
 }
